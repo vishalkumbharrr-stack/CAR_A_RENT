b12e762aac67dbe6f8d477696e3cec1aa2a99778
vishalkumbharrr-stack <vishalkumbharr@gmail.com>
2026-07-04 22:27:16 +0530

    Fix CORS for Vercel, add custom sound notifications, final cleanup

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # CORS - SABSE PEHLE ADD KARO
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://car-a-rent.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://car-a-rent.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
