f48d3eaab041737e8b4f92cd54218bf8e0c86d9b
Vishal Kumbhar <vishalkumbharrr@gmail.com>
2026-07-14 13:36:53 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://car-a-rent.vercel.app",
-        "https://new-url.vercel.app"   
+        "https://rentacar-git-main-kvishal.vercel.app"   
     ],
     allow_credentials=True,
     allow_methods=["*"],
