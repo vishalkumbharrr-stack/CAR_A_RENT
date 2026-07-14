5defbfe57d3da1084a427cdb3219bcb5d07d031d
Vishal Kumbhar <vishalkumbharrr@gmail.com>
2026-07-14 13:29:06 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://car-a-rent.vercel.app"   # no trailing slash
+        "https://car-a-rent.vercel.app",
+        "https://new-url.vercel.app"   
     ],
     allow_credentials=True,
     allow_methods=["*"],
